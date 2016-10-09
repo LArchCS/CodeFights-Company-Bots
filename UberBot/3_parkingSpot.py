239d97a1761ef47f5cf8d8f6066c88748b2a3ed2
LArchCS <fandi.inbox@gmail.com>
2016-10-09 19:48:13 -0400

    Update 3_parkingSpot.py

--- UberBot/3_parkingSpot.py
+++ UberBot/3_parkingSpot.py
@@ -2,18 +2,20 @@
 #  https://codefights.com/fight/HvAmyEvETRb7rs5zy
 
 def parkingSpot(carDimensions, parkingLot, luckySpot):
+    # for cases the shorter side of parking spot (entrance) is along the X axis
     def passCaseX(x1,x2,y1,y2, n):
         count = spotShort
         for x in range(x1, x2):
             for y in range(y1, y2, n):
                 if parkingLot[x][y] == 1:
-                    if x - x1 < carShort-1 and x2 - x < carShort-1:
+                    if x - x1 < carShort-1 and x2 - x < carShort-1: # if the obstacle lies in the middle of the entrance.. 
                         return False
                     count -= 1
                     break
-        return count >= carShort
+        return count >= carShort  # if the obstacles do not totally block the entrance for the car
 
     def passCaseY(x1,x2,y1,y2, n):
+        # for cases the parking spot entrance is along the Y axis
         count = spotShort
         for y in range(y1, y2):
             for x in range(x1, x2, n):
@@ -28,6 +30,8 @@ def parkingSpot(carDimensions, parkingLot, luckySpot):
     spotShort = min(luckySpot[2]-luckySpot[0]+1, luckySpot[3]-luckySpot[1]+1)
     
     if (luckySpot[2]-luckySpot[0]) <= (luckySpot[3]-luckySpot[1]):
+        # use <= just in cases of parking spots in squares (have 2 entrances)
+        # if == , if this test fail, go to the next test, since there are 2 entrances
         print "short at X\t",
         if (passCaseX(luckySpot[0],luckySpot[2]+1,0,luckySpot[3]+1, 1) or
             passCaseX(luckySpot[0],luckySpot[2]+1,len(parkingLot[0])-1,luckySpot[1]-1, -1)):
@@ -38,7 +42,7 @@ def parkingSpot(carDimensions, parkingLot, luckySpot):
         return (passCaseY(0, luckySpot[2]+1,luckySpot[1], luckySpot[3]+1, 1) or
             passCaseY(len(parkingLot)-1, luckySpot[0]-1,luckySpot[1], luckySpot[3]+1, -1))
             
-    return False
+    return False  # if only went into the first test, but failed
 
  
 
