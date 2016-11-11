45bcfac540b586015d6e37935d191bf9bd841a16
LArchCS <fandi.inbox@gmail.com>
2016-11-11 02:26:23 -0500

    Update 2_perfectCity.py

--- UberBot/2_perfectCity.py
+++ UberBot/2_perfectCity.py
@@ -3,11 +3,13 @@ def perfectCity(departure, destination):
     d2 = destination
     if abs(d1[0] - d2[0]) >= abs(d1[1] - d2[1]):
         r1 = abs(d1[0] - d2[0])
-        r2 = min((d1[1] + d2[1]), (abs(int(d2[1]+0.5)-d1[1])+ abs(int(d2[1]+0.5)-d2[1])), (abs(int(d1[1]+0.5)-d1[1])+ abs(int(d1[1]+0.5)-d2[1])))
+        r2 = min((d1[1] + d2[1]), (abs(int(d2[1] + 0.5) - d1[1])+ abs(int(d2[1] + 0.5) - d2[1])), 
+                 (abs(int(d1[1] + 0.5) - d1[1]) + abs(int(d1[1] + 0.5) - d2[1])))
         print 'A', r1, r2
     else:
         r1 = abs(d1[1] - d2[1])
-        r2 = min((d1[0] + d2[0]), (abs(int(d2[0]+0.5)-d1[0])+ abs(int(d2[0]+0.5)-d2[0])), (abs(int(d1[0]+0.5)-d1[0])+ abs(int(d1[0]+0.5)-d2[0])))
+        r2 = min((d1[0] + d2[0]), (abs(int(d2[0] + 0.5) - d1[0]) + abs(int(d2[0] + 0.5) - d2[0])),
+                 (abs(int(d1[0] + 0.5) - d1[0]) + abs(int(d1[0]+0.5)-d2[0])))
         print 'B', r1, r2
     return r1 + r2
     
